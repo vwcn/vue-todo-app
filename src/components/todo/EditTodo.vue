dab4e06a84265eb38f937a38cd6ff6346733197a
hhao99 <hhao99@gmail.com>
2024-12-26 23:00:01 -0800

    shadcning...

--- src/components/todo/EditTodo.vue
+++ src/components/todo/EditTodo.vue
@@ -1,56 +1,58 @@
 <script lang="ts" setup>
-import { ref } from 'vue'
+// import { ref } from 'vue'
 import type { Todo } from '@/stores/todo'
 import { useTodoStore } from '@/stores/todo'
-const props = defineProps<{todo: Todo, isNew: boolean}>();
-
-const task = ref(props.todo.text)
-const done = ref(props.todo.done)
+defineProps<{todo: Todo, isNew: boolean}>();
 const todos = useTodoStore()
 
-const handleSave = () => {
-  if (props.isNew) {
-    todos.add({ text: task.value, done: done.value })
-  } else {
-    todos.update({ ...props.todo, text: task.value, done: done.value })
-  }
-}
-
-// shadcn
-import { Button } from '@/components/ui/button'
+// shadcn form
 import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from '@/components/ui/card'
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormDescription,
+  FormMessage,
+} from '@/components/ui/form'
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+// form validation
+import { useForm } from 'vee-validate';
+import { toTypedSchema } from '@vee-validate/zod';
+import { z } from 'zod';
+
+
+
+const { handleSubmit } = useForm({ validationSchema: toTypedSchema(z.object({
+  task: z.string().min(4)
+})) });
+
+const onSubmit = handleSubmit(async (values) => {
+  console.log(values)
+})
+
+// shadcn@/components/ui/button'
+
 </script>
 <template>
-      <Card>
-    <CardHeader>
-      <CardTitle>Edit the task</CardTitle>
-      <CardDescription>editing the task.</CardDescription>
-    </CardHeader>
-    <CardContent>
-      <div>
-        <div>
-          <label>task: <input type='text' name='task' id='task' v-model='task' /></label>
-        </div>
-        <div>
-          mark it done?: <input type="checkbox" id='done'  v-model="done" />
-        </div>
-      </div>
-    </CardContent>
-    <CardFooter>
-      <div class="flex space-x-4 justify-end">
-        <Button variant='default' @click="handleSave">
-          <span v-if="isNew">Add</span>
-          <span v-else>Save</span>
-        </Button>
-        <Button variant='outline' @click="$emit('eddited')">Cancel</Button>
-      </div>
-    </CardFooter>
-  </Card>
+        <form @submit="onSubmit">
+        <FormField v-slot="{componentField }" name="task" :initialValue="'test2s'">
+            <FormItem>
+              <FormLabel for="task">New Task</FormLabel>
+              <FormControl>
+                <Input
+                  v-bind="componentField"
+                  type="text"
+                  placeholder="Enter new task"
+                />
+              </FormControl>
+              <FormDescription>
+          What do you want to do?
+        </FormDescription>
+        <FormMessage />
+            </FormItem>
+          </FormField>
+          <Button type="submit" variant="default" size="sm">Save</Button>
+        </form>
+
 </template>
